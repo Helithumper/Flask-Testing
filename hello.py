fd1c4c75c9f655d77d9773c00dc50722d6552acb
Peyton Duncan <peyton.duncan45@gmail.com>
2016-03-22 22:39:35 -0400

    toying with Flask

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+from app import app
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Peyton'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
 if __name__ == "__main__":
     app.run(host='45.55.180.111')
